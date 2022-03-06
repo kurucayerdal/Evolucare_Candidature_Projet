6539af99ed233db58cd4e3f63b98d37debfbf73b
kurucayerdal <kurucayerdal@gmail.com>
2022-03-06 21:50:29 +0100

    jenkins connection

---
+++ cypress/integration/TC_002/TC_2.spec.js
@@ -0,0 +1,7 @@
+///<reference types="cypress"/>
+
+describe('Example Test 1',()=>{
+    it('Example Test Case 1',()=>{
+        cy.visit('https://www.google.fr')
+    })
+})
---
+++ cypress/integration/deneme.js
@@ -0,0 +1,2 @@
+const myEmail = (Math.random() + 1).toString(36).substring(7) + "@gmail.com";
+console.log(myEmail);
